3c3271be279abbee01ec8124a99366056043fe52
David Olea <david@Davids-MacBook-Air.local>
2021-09-11 17:29:57 -0700

    figuring out the access token

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,17 @@
 var getApis = function(search) {
+    //this link is used to redirect the user to login. Right now the link redirects to one of my gitpages but once this page is properly running we switch the link to this page. Once redirected and authorized the url generates a new auth link which we need to replace the old expired one. (link expires every hour) 
+
+    //Im having trouble figuring out the best way to add this link and get the access code from the url 
+
+    var accessToken = ''
+
+    fetch("https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/git-it-done/&response_type=token", {mode: "no-cors"}).then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+
 
     fetch("https://api.spotify.com/v1/search?q=" + /*this will be the mood selector -> */"Sad" + 
 
@@ -8,7 +21,9 @@ var getApis = function(search) {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer BQDPcyBYJrBFUkoEF7KExnNUn7Hw3kmh9FUBpjGBHCOozlsyRPAlnsLbJicZArMXFLPgKBw88J-12zYwycO2jw7FtdXT69AyKXRGVjhZ-W5LvKZ4ROmQgn2RH1X5MBT49m_mXY4X7SGumiI'
+            // the var AccesToken would replace the random string that make up the old token 
+            'Authorization': 'Bearer BQClsVBfkkr89QrIq18NkTMzDndmxgmNtNJcM9a2cuIj2WX8tJCV2XUM9oH1cegGLfp0POCFbJ4lq7qZ4llwpRrtWuQIghx4sKFvYhgVkKytX4WlLaslo-Z4-Ztcr-j-OR1XOI8C4xOP9LWE9_uGLDp0cJe2A1Sd3u8KEd7HKbNeDdM',
+            
          }
     }) 
         .then(function(response) {
