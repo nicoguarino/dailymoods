f57d2b45b93899b4b4cc8e0dd40408b95ab1c113
David Olea <david@Davids-MacBook-Air.local>
2021-09-12 07:27:09 -0700

    added css chaning to match mood

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,61 +1,166 @@
-var buttonFunction = function(){
-let a = location.href = 'https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/git-it-done/&response_type=token'
 
-var b = a.split("=")
-var c =console.log(b[2])
+var moodSelectContainer = document.querySelector("#mood-btn");
 
-setTimeout(function(){
-    console.log(b[2])
-},3000)
 
-}
+var cssTheme = function(){
+    document.getElementById("mood-btn").addEventListener("click",function(){
+        var moodOptions = document.querySelector("#moods").value;
+        var linkArray = ["./assets/css/style.css","./assets/css/dark.css"]
+        if(moodOptions=="happy"){
+            document.getElementById("default").href="./assets/css/happy.css"
+        }
+        else if(moodOptions == "sad"){
+            document.getElementById("default").href="./assets/css/sad.css"
+        }
+        else if (moodOptions == "angry"){
+            document.getElementById("default").href="./assets/css/angry.css"
+        }
+        else if (moodOptions == "excited"){
+            document.getElementById("default").href="./assets/css/excited.css"
+        }
 
-var getApis = function(search) {
-    //this link is used to redirect the user to login. Right now the link redirects to one of my gitpages but once this page is properly running we switch the link to this page. Once redirected and authorized the url generates a new auth link which we need to replace the old expired one. (link expires every hour) 
-
-    //Im having trouble figuring out the best way to add this link and get the access code from the url 
-
-    var accessToken = ''
-
-    fetch("https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/git-it-done/&response_type=token", {mode: "no-cors"}).then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        var a = document.createElement("p")
-        a.innerHTML = data
-        console.log(data)
     })
 
-
-    fetch("https://api.spotify.com/v1/search?q=" + /*this will be the mood selector -> */"Sad" + 
-
-    "&type=" + /* this will be where the music options are selected -> */ "track%2Cartist%2Cplaylist"
-
-     + "&market=US&limit=5&offset=5", {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            // the var AccesToken would replace the random string that make up the old token 
-            'Authorization': 'Bearer BQBPtxy3aPMz8YFNytbOR-0VzlvlWIeq0FbilL8leY2pMwoudZUUBhfx3N_Efk2rE2WeD9zFeo3u72OAryNxPutcWz19CsbRsB-o0c5u4lnmkAXAXBhoBnq7NEyeaha68cHoV7C84JK2BXArFo2eqKDse1rqMzfhThWoaUPF_7mG71I',
-            
-         }
-    }) 
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-        });
-
-    var qouteApi = "https://goquotes-api.herokuapp.com/api/v1/all/quotes?New%20item=" + /* this will be the mood selector for quote api -> */ "sad";
-
-    fetch(qouteApi)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        });
+    
 };
 
-getApis();
+var getApis = function(moodOptions) {
+  
+ 
+   fetch("https://api.spotify.com/v1/search?q=" + /*this will be the mood selector -> */ moodOptions +
+ 
+   "&type=" + /* this will be where the music options are selected -> */ "playlist" /* Should we still give different options? */
+ 
+    + "&market=US&limit=10&offset=5", {
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': /* this is the token key -> */'Bearer BQCvPIZbdhow5qS6ieYlHCDMCI_rqsl3hT9jVHKglsum1iAuxSJmZNerijwXDPaWcVLsks2gGqotMO15gPuUuCK11JJNXNTBZZqNjaboOB_QGOwPmS9RMahYl_H0urGEhTrAY_3LlUhqWSyIk-amK3Gf-e3mxW9fJWbN0NGYuZ2dsPM'
+        }
+   })
+       .then(function(response) {
+           return response.json();
+       })
+       .then(function(data){
+           console.log(data);
+           console.log(moodOptions);
+ 
+           // runs spotifyDisplay function
+           spotifyDisplay(data);
+       });
+ 
+   var qouteApi = "https://goquotes-api.herokuapp.com/api/v1/all/quotes?New%20item=" + /* this will be the mood selector for quote api -> */ moodOptions;
+ 
+   fetch(qouteApi)
+       .then(function(response) {
+           return response.json();
+       })
+       .then(function(data) {
+           console.log(data);
+ 
+           quoteDisplay(data);
+       });
+};
+ 
+// Displays Spotify Iframe
+function spotifyDisplay (data) {
+ 
+   // clears spotify container
+   document.querySelector("#spotify-display").innerHTML = "";
+ 
+   // grabs spotify container
+  var spotifyContainer = document.querySelector('#spotify-display');
+ 
+   // dynamically creates iframe to display spotify embed code
+  var spotifyEmbedEl = document.createElement('iframe');
+  spotifyEmbedEl.setAttribute("src", "https://open.spotify.com/embed/playlist/" + data.playlists.items[Math.floor(Math.random() * 10)].id);
+  spotifyEmbedEl.setAttribute("allowtransparency", "true");
+  spotifyEmbedEl.setAttribute("allow", "encrypted-media");
+ 
+  //applies spotify css class
+  spotifyEmbedEl.classList = "spotify-embed-El";
+ 
+  // appendsChild iframe to spotify container
+  spotifyContainer.appendChild(spotifyEmbedEl);
+ 
+};
+ 
+// Displays Quote in H3
+function quoteDisplay (data) {
+   // clears quote container
+   document.querySelector("#quote-display").innerHTML = "";
+ 
+   // grabs quote container
+   var quoteContainer = document.querySelector('#quote-display');
+ 
+   // dynamically creates h2 to display quote generated
+   var quoteEl = document.createElement("h3")
+   quoteEl.textContent = '"' + data.quotes[Math.floor(Math.random() *200)].text + '"' + " -" + data.quotes[Math.floor(Math.random() *200)].author ;
+ 
+   quoteContainer.appendChild(quoteEl);
+ 
+};
+ 
+// Runs JavaScript on Selecting Value
+function moodSubmitHandler(event) {
+   event.preventDefault();
+ 
+   // pulls select value
+   var moodOptions = document.querySelector("#moods").value;
+ 
+   //runs getApi fetch function
+   getApis(moodOptions);
+ 
+   saveMood(moodOptions);
+ 
+};
+// Saves selected moods
+function saveMood(mood) {
+   var moodArr = JSON.parse(localStorage.getItem("saved-moods"))
+   || [];
+    moodArr.push(mood);
+    localStorage.setItem("saved-moods", JSON.stringify(moodArr));
+}
+ 
+// Shows history of moods selected
+function renderMoods() {
+   var moodArr = JSON.parse(localStorage.getItem("saved-moods"))
+   || [];
+   for (var i = 0; i < moodArr.length; i++) {
+       const chosenMood = moodArr[i];
+       console.log(chosenMood);
+       var li = $("<li>");
+       var button = $("<button>");
+       button.text(chosenMood);
+       button.addClass("history-btns");
+       li.append(button);
+       $("#mood-list").append(li);
+   }
+}
+ 
+renderMoods();
+ 
+$(".history-btns").click(function() {
+   var moodOptions  = ($(this)[0].innerText);
+    if(moodOptions=="happy"){
+       document.getElementById("default").href="./assets/css/happy.css"
+     }
+    else if(moodOptions == "sad"){
+    document.getElementById("default").href="./assets/css/sad.css"
+    }
+    else if (moodOptions == "angry"){
+        document.getElementById("default").href="./assets/css/angry.css"
+    }
+    else if (moodOptions == "excited"){
+        document.getElementById("default").href="./assets/css/excited.css"
+    }
+   
+ 
+   getApis(moodOptions);
+  
+})
+ 
+cssTheme()
+// on value select runs moodSubmitHandler function
+moodSelectContainer.addEventListener("click", moodSubmitHandler);
+
